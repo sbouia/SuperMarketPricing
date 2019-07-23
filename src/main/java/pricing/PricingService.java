1ada0f60f99ed2dd98c1f72e136e510be0061b5e
Amine Sboui <amine.sboui@zsoft-consulting.com>
2019-07-23 17:09:54 +0200

    implementation: calculate total for articles with promos

--- src/main/java/pricing/PricingService.java
+++ src/main/java/pricing/PricingService.java
@@ -12,17 +12,21 @@ public class PricingService {
     public double calculateTotal(Set<ArticleWithQuantity> articleWithQuantities) {
         return articleWithQuantities.stream()
                 .map(articleWithQuantity ->
-
-                        (articleWithQuantity.getQuantity() * articleWithQuantity.getArticle().getPrice()))
-
-
+                        bestPromo(articleWithQuantity.getArticle(), articleWithQuantity.getQuantity())
+                                .map(promo -> promo.calculate(
+                                        articleWithQuantity.getArticle().getPrice(),
+                                        articleWithQuantity.getQuantity()
+                                        )
+                                )
+                                .orElse(articleWithQuantity.getArticle().getPrice() * articleWithQuantity.getQuantity())
+                )
                 .reduce(0d, (total, subTotal) -> total + subTotal);
     }
 
     public Optional<Promo> bestPromo(Article article, double quantity) {
         return article.getPromos().stream().min((promo1, promo2) ->
-                (int)(promo1.calculate(quantity,article.getPrice()) -
-                promo2.calculate(quantity,article.getPrice()))
+                (int) (promo1.calculate(quantity, article.getPrice()) -
+                        promo2.calculate(quantity, article.getPrice()))
         );
     }
 }
