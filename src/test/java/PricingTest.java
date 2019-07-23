46ee0287e479f641560bf1ee1fa4cd34231e8044
Amine Sboui <amine.sboui@zsoft-consulting.com>
2019-07-23 17:00:46 +0200

    test: calculate total price for articles with promos

--- src/test/java/PricingTest.java
+++ src/test/java/PricingTest.java
@@ -4,6 +4,7 @@ import entities.Unity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import pricing.PricingService;
+import promotions.Promo;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,6 +14,9 @@ import java.util.Set;
 class PricingTest {
 
     private static PricingService pricingService = new PricingService();
+    private static Promo goodPromoMock = new GoodPromo();
+    private static Promo bestPromoMock = new BestPromo();
+    private static Promo mediocrePromoMock = new MediocrePromo();
 
     @Test
     void test() {
@@ -61,5 +65,29 @@ class PricingTest {
         //Then
         double expectedTotal = (3.2 * 100) / 16;
         Assertions.assertEquals(expectedTotal, result);
+    }
+
+    @Test
+    void should_return_total_for_articles_with_promos() {
+        //Given
+        Article article1 = new Article("article1", 100);
+        Article article2 = new Article("article2", 50);
+
+
+        article1.getPromos().add(bestPromoMock);
+        article1.getPromos().add(goodPromoMock);
+        article1.getPromos().add(mediocrePromoMock);
+
+        article2.getPromos().add(bestPromoMock);
+        article2.getPromos().add(goodPromoMock);
+        article2.getPromos().add(mediocrePromoMock);
+
+        ArticleWithQuantity articleWithQuantity1 = new ArticleWithQuantity(article1, 3);
+        ArticleWithQuantity articleWithQuantity2 = new ArticleWithQuantity(article2, 2);
+        Set<ArticleWithQuantity> articleWithQuantities = new HashSet<>(Arrays.asList(articleWithQuantity1, articleWithQuantity2));
+        //When
+        double result = pricingService.calculateTotal(articleWithQuantities);
+        //Then
+        Assertions.assertEquals(2, result);
     }
 }
