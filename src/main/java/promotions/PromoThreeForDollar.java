68db37313936499d93aaed370336f385bf43954e
Amine Sboui <amine.sboui@zsoft-consulting.com>
2019-07-23 15:42:28 +0200

    Refactoring code method calculate price 3 for 1 dollar promo

--- src/main/java/promotions/PromoThreeForDollar.java
+++ src/main/java/promotions/PromoThreeForDollar.java
@@ -3,8 +3,6 @@ package promotions;
 public class PromoThreeForDollar implements Promo {
     @Override
     public double calculate(double articleNbr, double unitPrice) {
-        if (articleNbr == 3)
-            return 1;
         return (articleNbr % 3) * unitPrice + (int) (articleNbr / 3);
     }
 }
