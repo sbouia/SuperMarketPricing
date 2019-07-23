4fb2af0a7a0b836d6a27f8bb978f59587442aebe
Amine Sboui <amine.sboui@zsoft-consulting.com>
2019-07-23 15:47:56 +0200

    Test: calculate promo buy 2 get 1 for free

--- src/test/java/PromoTest.java
+++ src/test/java/PromoTest.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import promotions.Promo;
+import promotions.PromoBuyTwoGetOneFree;
 import promotions.PromoThreeForDollar;
 
 class PromoTest {
 
     private static Promo promoThreeForDollar = new PromoThreeForDollar();
+    private static Promo promoBuyTwoGetOneFree = new PromoBuyTwoGetOneFree();
 
     @Test
     void should_return_one_dollar_for_three_articles() {
@@ -28,6 +30,17 @@ class PromoTest {
         double result = promoThreeForDollar.calculate(quantityBought, unitPrice);
         //Then
         Assertions.assertEquals(2, result);
+    }
+
+    @Test
+    void should_return_price_of_two_when_buying_three() {
+        //Given
+        double articleNbr = 3;
+        double unitPrice = 0.5;
+        //When
+        double result = promoBuyTwoGetOneFree.calculate(articleNbr, unitPrice);
+        //Then
+        Assertions.assertEquals(1, result);
     }
 
 }
